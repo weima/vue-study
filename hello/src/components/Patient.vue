1ebfd7e0da84a32545f6530ca786101a6752d6b2
Wei Ma <weima@localhost.localdomain>
2019-01-17 22:03:10 -0800

    Added the first vue thing

---
+++ hello/src/components/Patient.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="patient">Hi Patient</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Patient extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
---
+++ hello/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="This is my Hello"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Patient from "@/components/Patient.vue";
+
+@Component({
+  components: {
+    Patient
+  }
+})
+export default class Home extends Vue {}
+</script>
